e9f85baac008c53e72ed8505dceeb791264c7a39
cran-cg <guptachiranjeev.13@gmail.com>
2018-09-18 03:13:58 +0530

    chore: add monaco to the app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as monaco from 'monaco-editor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'editor';
+  editor: any;
+  nativeContainer: any;
+  ngOnInit(): void {
+    this.nativeContainer = document.getElementById('container');
+    if (this.nativeContainer) {
+      this.editor = monaco.editor.create(this.nativeContainer, {language: 'java'});
+    }
+  }
 }
